3ed0e0c5badc434c435f1a318ad7fcf04f12f8ef
Brennan Maloney <brennan.maloney02@gmail.com>
2023-08-23 19:29:06 -0400

    changed dictionary and element names

--- ChessTest/MainWindow.xaml.cs
+++ ChessTest/MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace ChessTest
         private Point startPosition;
         private Point originalPosition;
         private ImageSource originalSource;
-        private Dictionary<Image, ImageSource> pieceImageSources = new Dictionary<Image, ImageSource>();
+        private Dictionary<string, ImageSource> pieceImageSources = new Dictionary<String, ImageSource>();
 
         public MainWindow()
         {
@@ -36,37 +36,77 @@ namespace ChessTest
 
         private void WindowLoaded(object sender, RoutedEventArgs e)
         {
-            pieceImageSources[BPawn1] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BPawn2] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BPawn3] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BPawn4] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BPawn5] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BPawn6] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BPawn7] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BRook1] = new BitmapImage(new Uri("/Images/BlackRook.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BRook2] = new BitmapImage(new Uri("/Images/BlackRook.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BKnight1] = new BitmapImage(new Uri("/Images/BlackKnight.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BKnight2] = new BitmapImage(new Uri("/Images/BlackKnight.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BBishop1] = new BitmapImage(new Uri("/Images/BlackBishop.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BBishop2] = new BitmapImage(new Uri("/Images/BlackBishop.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BKing] = new BitmapImage(new Uri("/Images/BlackKing.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[BQueen] = new BitmapImage(new Uri("/Images/BlackQueen.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["A8"] = new BitmapImage(new Uri("/Images/BlackRook.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["B8"] = new BitmapImage(new Uri("/Images/BlackKnight.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["C8"] = new BitmapImage(new Uri("/Images/BlackBishop.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["D8"] = new BitmapImage(new Uri("/Images/BlackQueen.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["E8"] = new BitmapImage(new Uri("/Images/BlackKing.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["F8"] = new BitmapImage(new Uri("/Images/BlackBishop.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["G8"] = new BitmapImage(new Uri("/Images/BlackKnight.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["H8"] = new BitmapImage(new Uri("/Images/BlackRook.png", UriKind.RelativeOrAbsolute));
 
-            pieceImageSources[WPawn1] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WPawn2] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WPawn3] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WPawn4] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WPawn5] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WPawn6] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WPawn7] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WRook1] = new BitmapImage(new Uri("/Images/WhiteRook.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WRook2] = new BitmapImage(new Uri("/Images/WhiteRook.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WKnight1] = new BitmapImage(new Uri("/Images/WhiteKnight.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WKnight2] = new BitmapImage(new Uri("/Images/WhiteKnight.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WBishop1] = new BitmapImage(new Uri("/Images/WhiteBishop.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WBishop2] = new BitmapImage(new Uri("/Images/WhiteBishop.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WKing] = new BitmapImage(new Uri("/Images/WhiteKing.png", UriKind.RelativeOrAbsolute));
-            pieceImageSources[WQueen] = new BitmapImage(new Uri("/Images/WhiteQueen.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["A7"] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["B7"] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["C7"] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["D7"] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["E7"] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["F7"] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["G7"] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["H7"] = new BitmapImage(new Uri("/Images/BlackPawn.png", UriKind.RelativeOrAbsolute));
+
+            pieceImageSources["A6"] = null;
+            pieceImageSources["B6"] = null;
+            pieceImageSources["C6"] = null;
+            pieceImageSources["D6"] = null;
+            pieceImageSources["E6"] = null;
+            pieceImageSources["F6"] = null;
+            pieceImageSources["G6"] = null;
+            pieceImageSources["H6"] = null;
+
+            pieceImageSources["A5"] = null;
+            pieceImageSources["B5"] = null;
+            pieceImageSources["C5"] = null;
+            pieceImageSources["D5"] = null;
+            pieceImageSources["E5"] = null;
+            pieceImageSources["F5"] = null;
+            pieceImageSources["G5"] = null;
+            pieceImageSources["H5"] = null;
+
+            pieceImageSources["A4"] = null;
+            pieceImageSources["B4"] = null;
+            pieceImageSources["C4"] = null;
+            pieceImageSources["D4"] = null;
+            pieceImageSources["E4"] = null;
+            pieceImageSources["F4"] = null;
+            pieceImageSources["G4"] = null;
+            pieceImageSources["H4"] = null;
+
+            pieceImageSources["A3"] = null;
+            pieceImageSources["B3"] = null;
+            pieceImageSources["C3"] = null;
+            pieceImageSources["D3"] = null;
+            pieceImageSources["E3"] = null;
+            pieceImageSources["F3"] = null;
+            pieceImageSources["G3"] = null;
+            pieceImageSources["H3"] = null;
+
+            pieceImageSources["A2"] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["B2"] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["C2"] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["D2"] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["E2"] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["F2"] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["G2"] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["H2"] = new BitmapImage(new Uri("/Images/WhitePawn.png", UriKind.RelativeOrAbsolute));
+
+            pieceImageSources["A1"] = new BitmapImage(new Uri("/Images/WhiteRook.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["B1"] = new BitmapImage(new Uri("/Images/WhiteKnight.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["C1"] = new BitmapImage(new Uri("/Images/WhiteBishop.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["D1"] = new BitmapImage(new Uri("/Images/WhiteQueen.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["E1"] = new BitmapImage(new Uri("/Images/WhiteKing.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["F1"] = new BitmapImage(new Uri("/Images/WhiteBishop.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["G1"] = new BitmapImage(new Uri("/Images/WhiteKnight.png", UriKind.RelativeOrAbsolute));
+            pieceImageSources["H1"] = new BitmapImage(new Uri("/Images/WhiteRook.png", UriKind.RelativeOrAbsolute));
         }
 
         private void SquareMouseDown(object sender, MouseButtonEventArgs e)
@@ -98,17 +138,20 @@ namespace ChessTest
 
         private void SquareMouseUp(object sender, MouseButtonEventArgs e)
         {
+            Grid clickedSquare = sender as Grid;
+            if(clickedSquare != null)
+            {
+                string squareName = clickedSquare.Name;
+
+                Debug.WriteLine(squareName);
+            }
             if(isDragging && draggedPiece != null)
             {
-                originalImage.Source = originalSource;
-
-                //ImageSource tempSource = pieceImageSources[originalImage];
-
-                //pieceImage.Source = tempSource;
-
-
-
-
+                if(originalImage != null)
+                {
+                    originalImage.Source = originalSource;
+                    Debug.WriteLine(originalImage.Source);
+                }
                 draggedPiece = null;
                 isDragging = false;
                 pieceImage = null;
